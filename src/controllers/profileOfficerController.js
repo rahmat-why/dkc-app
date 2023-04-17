b18a14a473dbae2c65473352ba36f05157db3283
rizkymuliap <perdanarizkymulia162@gmail.com>
2023-04-17 20:02:00 +0700

    update

--- src/controllers/profileOfficerController.js
+++ src/controllers/profileOfficerController.js
@@ -10,6 +10,16 @@ export async function getAll(req, res) {
     }
 }
 
+export async function getByScope(req, res) {
+    try {
+        const { scope_id } = req.params
+        const profile_officer = await profileOfficerModel.getByScope(scope_id);
+        return response(res, 200, true, "Success", profile_officer)
+    }catch(e) {
+        return response(res, 500, false, e, {})
+    }
+}
+
 export async function store(req, res) {
     try {
         const { name, nta, stage_id, scope_id, education, city, instagram, facebook } = req.body
