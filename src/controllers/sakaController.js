9372f7b1d469f76b171ccb1a8595dccbc6c60ef0
unknown <perdanarizkymulia162@gmail.com>
2023-09-17 10:24:56 +0700

    model log activity document saka & pin saka

--- src/controllers/sakaController.js
+++ src/controllers/sakaController.js
@@ -1,5 +1,6 @@
 import response from "../response.js";
 import * as sakaModel from "../models/sakaModel.js";
+import * as logDocumentSaka from "../models/logDocumentSKSaka.js"
 
 export async function getAll(req, res) {
     try {
@@ -25,12 +26,16 @@ export async function uploadDocumentSkSaka(req, res) {
     try {
         const { saka_id } = req.params;
         const { filename } = req.file;
-        
+        const year = new Date().getFullYear().toString();
+
+        console.log(year);
+
         // Buat URL untuk file yang diunggah
         const document_sk_saka_url = "/sk_saka/" + filename;
 
         // Panggil model untuk memperbarui kolom `document_sk_saka` pada Saka yang sesuai
         const update = await sakaModel.updateDocumentSkSaka(saka_id, document_sk_saka_url);
+        await logDocumentSaka.storeSKSaka(saka_id, document_sk_saka_url, year, 'SK SAKA' );
 
         return response(res, 200, true, "Document SK Saka successfully uploaded!", update);
     } catch (e) {
@@ -42,12 +47,14 @@ export async function uploadDocumentSkPinsaka(req, res) {
     try {
         const { saka_id } = req.params;
         const { filename } = req.file;
+        const year = new Date().getFullYear().toString();
         
         // Buat URL untuk file yang diunggah
         const document_sk_pinsaka_url = "/sk_pinsaka/" + filename;
 
         // Panggil model untuk memperbarui kolom `document_sk_saka` pada Saka yang sesuai
         const update = await sakaModel.updateDocumentSkPinsaka(saka_id, document_sk_pinsaka_url);
+        await logDocumentSaka.storeSKPinSaka(saka_id, document_sk_pinsaka_url, year, 'SK PINSAKA' )
 
         return response(res, 200, true, "Document SK Pin Saka successfully uploaded!", update);
     } catch (e) {
--- src/models/logDocumentSKSaka.js
+++ src/models/logDocumentSKSaka.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import connection from '../config/db.config.js'
 import { Saka } from "./sakaModel.js";
 
-export const LogDocumentSKSaka = connection.define('log_document_sksakas', {
+export const LogDocumentSKSaka = connection.define('log_document_sakas', {
     document_id: {
         primaryKey: true,
         type: Sequelize.STRING,
@@ -15,7 +15,38 @@ export const LogDocumentSKSaka = connection.define('log_document_sksakas', {
 
 LogDocumentSKSaka.belongsTo(Saka, { foreignKey: 'saka_id' });
 
-export const store = async (saka_id, document, year, type) => {
+export const storeSKSaka = async (saka_id, document, year, type) => {
+    // Check if a record with the same dkr_id and year exists
+    const existingRecord = await LogDocumentSKSaka.findOne({
+        where: {
+            saka_id: saka_id,
+            year: year,
+            type: type
+        }
+    });
+
+    if (existingRecord) {
+        // If a record exists, update it
+        await existingRecord.update({
+            document: document,
+            type: type,
+        });
+        return existingRecord; // Return the updated record
+    } else {
+        // If no record exists, create a new one
+        const document_id = "DCM" + Math.random();
+        const newRecord = await LogDocumentSKSaka.create({
+            document_id: document_id,
+            saka_id: saka_id,
+            document: document,
+            year: year,
+            type: type,
+        });
+        return newRecord; // Return the newly created record
+    }
+};
+
+export const storeSKPinSaka = async (saka_id, document, year, type) => {
     // Check if a record with the same dkr_id and year exists
     const existingRecord = await LogDocumentSKSaka.findOne({
         where: {
