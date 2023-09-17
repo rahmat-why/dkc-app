e2752cfba5036238330e26f4e9be2d42dca2e6d0
unknown <perdanarizkymulia162@gmail.com>
2023-09-17 11:39:31 +0700

    update db config

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const connection = new Sequelize("dev_dkc", "kabbogor", "Dkcdkc111", {
+const connection = new Sequelize("db_dkc", "kabbogor", "Dkcdkc111", {
     host: "103.175.216.90",
     dialect: 'mysql',
     pool: {
